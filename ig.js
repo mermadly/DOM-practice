3cc4ff6ffaaf0815e6438015a54e0b81dc2ab1fa
mermadly <34113249+mermadly@users.noreply.github.com>
2019-03-27 10:36:26 -0300

    prompt fail

--- ig.js
+++ ig.js
@@ -1,18 +1,27 @@
 var iconoheart = document.querySelectorAll(".interact .fa-heart")
-console.log (iconoheart)
-
 var clicks = 0;
 var likes = 0;
 
 function fillHeart(fill) {
-    
-    console.log(this)
     clicks++ 
     if (clicks%2 === 0) {
         likes++
         var numeroDeLikes = document.querySelectorAll('.likes')
-        numeroDeLikes.innerHTML = likes
+        numeroDeLikes[0].innerHTML = likes
     }
     
     fill.classList.toggle('fas')
+}   
+
+function comment () {
+    console.log ('blah')
+    var prompt = prompt('Add a comment:', '')
+    var commentSt = document.querySelector('.card')
+    var newElement = document.createElement('div')
+    commentSt.appendChild(newElement)
+    newElement.classList.add ('comments')
+    newElement.appendChild('p')
+    newElement.children[0].innerHTML = 'username'
+    newElement.appendChild('input')
+    newElement.children[1].value = prompt
 }
