2a884c79a43773e26a0ca126d7b8ee44395d67aa
mermadly <34113249+mermadly@users.noreply.github.com>
2019-06-02 15:21:31 -0300

    Instagram updates

--- ig.js
+++ ig.js
@@ -1,29 +1,51 @@
 var iconoheart = document.querySelectorAll(".interact .fa-heart")
 var clicks = 0;
-var likes = 0;
+var likes = [0, 0, 0]
 
 function fillHeart(fill) {
-    clicks++ 
-    if (clicks%2 === 0) {
-        likes++
-        var numeroDeLikes = document.querySelectorAll('.likes')
-        numeroDeLikes[0].innerHTML = likes
-    }
+
+    const index = [...iconoheart].indexOf(event.target);
+    likes[index]++
+
+    var numeroDeLikes = document.querySelectorAll('.likes')
+    numeroDeLikes[index].innerHTML = likes[index]
     
+
     fill.classList.toggle('fas')
-    fill.classList.toggle ('red')
 }   
 
-function comment (e) {
-    var promptComment = prompt('Add a comment:', '')
-    var commentSt = document.querySelectorAll('.commentSection')    
-    var newDiv = document.createElement('div')
-    var newP = document.createElement('p')
-    var newInput = document.createElement('input')
-    commentSt[0].appendChild(newDiv)
-    newDiv.classList.add ('comment')
-    newDiv.appendChild(newP)
-    newDiv.children[0].innerHTML = 'username'
-    newDiv.appendChild(newInput)
-    newDiv.children[1].value = promptComment
+function comment () {
+    
+    var inputListen = document.querySelector('.add');
+    
+    inputListen.addEventListener ('keydown', function () {
+        if (event.charCode === 13) {
+            console.log ('hola')
+
+        }
+    })
+
+        var promptComment = prompt('Add a comment:', '')
+        var commentSt = document.querySelectorAll('.commentSection')    
+        var newDiv = document.createElement('div')
+        var newP = document.createElement('p')
+        var newInput = document.createElement('input')
+        // var index = [...commentSt].indexOf('.commentSection')
+        // console.log(index)
+        commentSt[2].appendChild(newDiv)
+        newDiv.classList.add ('comment')
+        newDiv.appendChild(newP)
+        newDiv.children[0].innerHTML = 'username'
+        newDiv.appendChild(newInput)
+        newDiv.children[1].value = promptComment
+
 }
+
+// var toggleCeleste = 
+
+var buttonBlue = document.querySelector('button')
+inputListen = document.querySelector('.add')
+
+inputListen.addEventListener('keydown', function () {
+    buttonBlue.classList.add('blue')
+})
